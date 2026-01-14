02919fec6f154c90277563ca2a606af799a6a6d9
diegorego514-ship-it <diegorego514@gmail.com>
2026-01-13 23:05:58 -0300

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -13,9 +13,27 @@ try:
 except:
         def port_scanner(ip, port):
             def time_sleep():
-             time_sleep(10)
+             time_sleep(10) # Time in seconds
              p = port 
              p.scan(ip, port)
-        print(f"[+] Your port is vulnerable to hackers")
+        print(f"[+] Your Port is vulnerable to hackers")
 else:
-        print(f"[-] Your port is secure")
+        print(f"[-] Your Port is secure")
+
+try:
+     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+     s.settimeout(5)
+     def ip():
+          def port():
+               s.connect_ex((ip, port))
+               s.close()
+               return s
+except:
+            def port_scanner(ip, port):
+              def time_sleep():
+               time_sleep(15) # Time in seconds
+               p = port
+               p.scan(ip, port)
+            print(f"[+] Your Port has been intercepted by hackers")
+else:
+            print(f"[-] Your Port is not vulnerable to hackers")
